0eba92a3e75103433448eb41f5ef773cb8d00b3b
rabbitfishy <144937211+rabbitfishy@users.noreply.github.com>
2024-10-16 00:23:31 +0100

    Update form.cpp

--- core/gui/elements/form.cpp
+++ core/gui/elements/form.cpp
@@ -1,6 +1,7 @@
 #include "../../../includes.h"
 
 void Form::update_opacity( ) {
+
 	// opacity should reach 1 in 500 milliseconds.
 	constexpr float frequency = 1.f / 0.5f;
 
@@ -18,33 +19,38 @@ void Form::update_opacity( ) {
 }
 
 void Form::draw_background( int x, int y, int width, int height, Color color ) {
+
 	// draw background.
 	render::rect_filled( x, y, width, height, color );
 
-	// border colors.
-	static const std::array<Color, 5> border_colors = {
-		Color{5, 5, 5, m_alpha},
-		Color{60, 60, 60, m_alpha},
-		Color{40, 40, 40, m_alpha},
-		Color{40, 40, 40, m_alpha},
-		Color{60, 60, 60, m_alpha}
-	};
+	// skeet rgb line.
+	render::gradient( x, y + 1, width / 2, 1, Color( 99, 160, 200 ), Color( 179, 102, 181 ), true );
+	render::gradient( x + ( width / 2 ), y + 1, width / 2, 1, Color( 179, 102, 181 ), Color( 230, 217, 100 ), true );
 
-	// draw border.
-	for ( size_t i = 0; i < border_colors.size( ); ++i ) {
-		render::rect( x + i, y + i, width - 2 * i, height - 2 * i, border_colors[ i ] );
-	}
+	render::gradient( x, y + 2, width / 2, 1, Color( 49, 79, 99 ), Color( 89, 50, 90 ), true );
+	render::gradient( x + ( width / 2 ), y + 2, width / 2, 1, Color( 89, 50, 90 ), Color( 114, 108, 49 ), true );
+
+	// border.
+	// 					   ìnner,  middle, outter, border
+	int border_colors[ 6 ] = { 60, 35, 35, 35, 60, 0 };
+	for ( int i = 0; i < 6; i++ )
+		render::rect( x - i, y - i, width + 2 * i, height + 2 * i, Color( border_colors[ i ], border_colors[ i ], border_colors[ i ] ) );
 }
 
 void Form::draw_tabs( ) {
+
 	// check if we have any tabs.
-	if ( m_tabs.empty( ) ) return;
+	if ( m_tabs.empty( ) ) 
+		return;
 
 	// get the tabs area.
 	Rect tabs_area = GetTabsRect( );
 
-	// draw the background.
-	draw_background( tabs_area.x, tabs_area.y, tabs_area.w, tabs_area.h, { 17, 17, 17, m_alpha } );
+	// draw background.
+	render::rect_filled( tabs_area.x, tabs_area.y, tabs_area.w, tabs_area.h, { 17, 17, 17, m_alpha } );
+
+	// draw outline.
+	render::rect( tabs_area.x, tabs_area.y, tabs_area.w, tabs_area.h, { 17, 17, 17, m_alpha } );
 
 	Color color = g_gui.m_color;
 	color.a( ) = m_alpha;
@@ -55,21 +61,24 @@ void Form::draw_tabs( ) {
 		const auto& t = m_tabs[ i ];
 
 		// draw the tab title.
-		render::menu_shade.string( tabs_area.x + 10, tabs_area.y + 5 + ( i * 16 ),
-								   t == m_active_tab ? color : Color{ 152, 152, 152, m_alpha },
-								   t->m_title );
+		render::menu_shade.string( tabs_area.x + 10, tabs_area.y + 5 + ( i * 16 ), t == m_active_tab ? color : Color{ 152, 152, 152, m_alpha }, t->m_title );
 	}
 }
 
 void Form::draw_elements( ) {
+
 	// check if we have an active tab.
-	if ( m_active_tab->m_elements.empty( ) ) return;
+	if ( m_active_tab->m_elements.empty( ) ) 
+		return;
 
 	// elements background and border.
 	Rect el = GetElementsRect( );
 
-	// draw the background.
-	draw_background( el.x, el.y, el.w, el.h, { 17, 17, 17, m_alpha } );
+	// draw background.
+	render::rect_filled( el.x, el.y, el.w, el.h, { 17, 17, 17, m_alpha } );
+
+	// draw outline.
+	render::rect( el.x, el.y, el.w, el.h, { 17, 17, 17, m_alpha } );
 
 	// draw the date and username.
 	std::string date = XOR( __DATE__ );
@@ -92,8 +101,11 @@ void Form::draw_elements( ) {
 }
 
 void Form::draw( ) {
+
 	update_opacity( );
-	if ( !m_alpha ) return;
+
+	if ( !m_alpha ) 
+		return;
 
 	draw_background( m_x, m_y, m_width, m_height, { 12, 12, 12, m_alpha } );
 
